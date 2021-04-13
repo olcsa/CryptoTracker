c244eb273334a476c785baae00e42ceb5d8643be
Oliver Vasvari <oliwasi@gmail.com>
2021-04-13 21:51:01 +0200

    moves MainActivity to activities folder
    To make project more organized

--- app/src/main/java/com/oliwasi/cryptotracker/activities/MainActivity.java
+++ app/src/main/java/com/oliwasi/cryptotracker/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.oliwasi.cryptotracker;
+package com.oliwasi.cryptotracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.oliwasi.cryptotracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
